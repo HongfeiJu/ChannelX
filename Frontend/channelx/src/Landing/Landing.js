57afaad7529c92f57707da74a0e00fadbf12a204
MiriyalaM <mmiriyal@asu.edu>
2019-09-20 03:37:31 -0700

    US #6 Task #36 Basic commit for react js research

--- Frontend/channelx/src/Landing/Landing.js
+++ Frontend/channelx/src/Landing/Landing.js
@@ -1,11 +1,19 @@
 import React from 'react';
+Class LandingPage extends React.Component {
+
+onClickMethod = () => {
+console.log('Registration is clicked.');
+}
 
 const landing = () => {
     return (
         <div>
-            landing page
+            Welcome to ChannelX
         </div>
+        <a href="#" onClick={onClickMethod}>
+      	Register
+    	</a>
     );
 };
-
+}
 export default landing;
