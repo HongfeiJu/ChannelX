9f775c6c30a893258932b557ae9e3a0d9319a3f0
MiriyalaM <mmiriyal@asu.edu>
2019-09-24 02:00:10 -0700

    Changes in Landing page

--- Frontend/channelx/src/Landing/Landing.js
+++ Frontend/channelx/src/Landing/Landing.js
@@ -1,18 +1,32 @@
 import React from 'react';
 Class LandingPage extends React.Component {
 
-onClickMethod = () => {
-console.log('Registration is clicked.');
-}
 
 const landing = () => {
     return (
         <div>
             Welcome to ChannelX
         </div>
-        <a href="#" onClick={onClickMethod}>
-      	Register
-    	</a>
+         <div className="Register">
+                        <button
+                            type="button"
+                            className="Register"
+                            onClick={() => this.routeTo('././SignUp/SignUp')}
+                        >Register</button>
+        </div>
+       </div>
+        <div className="Login">
+                        <button
+                            type="button"
+                            className="Login"
+                            //onClick={() => this.routeTo('https://github.com/')}
+                        >Login</button>
+        </div>
+        <div className="About">
+                        <button
+                           <p> ChannelX will make it possible to create, share, and destroy transient communication channels. These channels should be tied to users' existing communication streams (e.g., SMS, e-mail, etc.) without exposing the user's actual phone number or e-mail address. </p>
+        </div>
+
     );
 };
 }
