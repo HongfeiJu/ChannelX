7ce37c69f530906651b444aad4cfe4acd9c847e4
Muhammad Sami <msami2@asu.edu>
2019-09-24 12:46:24 -0700

    Updating home page and styling login button

--- Frontend/channelx/src/Home/Home.js
+++ Frontend/channelx/src/Home/Home.js
@@ -1,6 +1,6 @@
 /*
 Description: Landing page
-Authors: Darshan Prakash
+Authors: Darshan Prakash, Sami
 Date: 9/24/2019
 */
 
@@ -8,8 +8,29 @@ import React, {Component} from 'react';
 import fire from "../config/Fire";
 
 class Home extends Component{
-
+    constructor(props){
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+    
+    logout(){
+        fire.auth().signOut();
+    }
+    
+    render() {
+        return (
+            <div className={"Home"}>
+                <h1>
+                You are in Home!
+                </h1>
+                <button onClick={this.logout}> Sign out
+                </button>
+            </div>
+        );
+    }
 
 }
+
+
 
 export default Home;
