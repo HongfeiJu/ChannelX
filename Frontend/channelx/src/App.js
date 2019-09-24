af7896598278bc04c4721fbb0d17444f05ad44a5
Darshan Prakash <darshan@darshans-mbp-3.3521.dhcp.asu.edu>
2019-09-24 12:29:35 -0700

    adding user registration feature from signup and adding sign out feature.

--- Frontend/channelx/src/App.js
+++ Frontend/channelx/src/App.js
@@ -1,25 +1,49 @@
 /*
 Description: Basic routing for landing page and register page
-Author: Hongfei Ju
+Author: Hongfei Ju, Darshan Prakash
 Date: 9/20/2019
 */
 
-import React from 'react';
+import React,{Component} from 'react';
 import './App.css';
 import Signup from './Signup/Signup';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Landing from './Landing/Landing';
+import fire from "./config/Fire";
+import Home from "./Home/Home";
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Route path="/" exact component={Landing}/>
-        <Route path="/signup" exact component={Signup}/>
-      </div>
-    </Router>
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            user:{}
+        };
+    }
 
-  );
+    componentDidMount() {
+        this.authlistener();
+    }
+
+    authlistener() {
+        fire.auth().onAuthStateChanged((user) => {
+            console.log(user);
+            if(user) {
+                this.setState({user});
+            } else {
+                this.setState({user: null});
+            }
+        });
+    }
+
+    render() {
+        return (
+            <div className={"App"}>
+                {this.state.user ?(<Home />):(<Signup />)}
+            </div>
+        );
+    }
 }
+
+
 
 export default App;
---
+++ Frontend/channelx/src/Home/Home.js
@@ -0,0 +1,15 @@
+/*
+Description: Landing page
+Authors: Darshan Prakash
+Date: 9/24/2019
+*/
+
+import React, {Component} from 'react';
+import fire from "../config/Fire";
+
+class Home extends Component{
+
+
+}
+
+export default Home;
