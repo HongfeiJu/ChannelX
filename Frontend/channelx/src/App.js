6c15697cdab0f2c84afd419a31b7bd11ca9e92cb
MiriyalaM <mmiriyal@asu.edu>
2019-09-25 18:55:49 -0700

    Landing page with buttons and basic layout

--- Frontend/channelx/src/App.js
+++ Frontend/channelx/src/App.js
@@ -1,18 +1,27 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Signup from './Signup/Signup';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Landing from './Landing/Landing';
+import Signup from './Signup/Signup';
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Route path="/" exact component={Landing}/>
-        <Route path="/signup" exact component={Signup}/>
-      </div>
+    <div className="App">
+      <Route path="/" exact component={Landing}/>
+      <Route path="/signup" exact component={Signup}/>
+        <a
+          className="App-link"
+          href="https://www.google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Register
+          </a>
+    </div>
     </Router>
-
   );
 }
 
--- Frontend/channelx/src/Landing/Landing.js
+++ Frontend/channelx/src/Landing/Landing.js
@@ -1,33 +1,47 @@
+/*
+Description: Landing page
+Authors: Manisha Miriyala
+Date: 9/20/2019
+*/
+
 import React from 'react';
-Class LandingPage extends React.Component {
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, ButtonGroup, ButtonToolbar } from 'reactstrap';
+import { Link } from 'react-router';
 
+/*import Button from 'react-bootstrap-button-loader';*/
+class Landing extends React.Component {
 
-const landing = () => {
+render(){
     return (
         <div>
-            Welcome to ChannelX
-        </div>
+        <header>
+           <h1> Welcome to ChannelX </h1>
+        </header>
          <div className="Register">
                         <button
                             type="button"
+                            style={{ marginLeft: "auto" }}
                             className="Register"
-                            onClick={() => this.routeTo('././SignUp/SignUp')}
+                            onClick={() => this.routeTo('./SignUp/SignUp')}
+                            
                         >Register</button>
         </div>
-       </div>
+   
         <div className="Login">
                         <button
                             type="button"
+                            style={{ marginLeft: "auto" }}
                             className="Login"
                             //onClick={() => this.routeTo('https://github.com/')}
                         >Login</button>
         </div>
-        <div className="About">
-                        <button
-                           <p> ChannelX will make it possible to create, share, and destroy transient communication channels. These channels should be tied to users' existing communication streams (e.g., SMS, e-mail, etc.) without exposing the user's actual phone number or e-mail address. </p>
-        </div>
+        <div>
 
+           <p> ChannelX will make it possible to create, share, and destroy transient communication channels. These channels should be tied to users existing communication streams (e.g., SMS, e-mail, etc.) without exposing the users actual phone number or e-mail address.</p>        </div>
+
+</div>
     );
 };
 }
-export default landing;
+export default Landing;
