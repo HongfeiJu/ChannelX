43e72d902e1b56eda4fd5bcb95104794fe897d6f
MiriyalaM <mmiriyal@asu.edu>
2019-09-26 21:13:41 -0700

    Landing page with navigation

--- Frontend/channelx/src/Landing/Landing.js
+++ Frontend/channelx/src/Landing/Landing.js
@@ -1,17 +1,48 @@
 /*
 Description: Landing page
-Authors: Hongfei Ju
+Authors: Manisha Miriyala
 Date: 9/20/2019
 */
 
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button, ButtonGroup, ButtonToolbar } from 'reactstrap';
+import { Link } from 'react-router';
 
-const landing = () => {
+/*import Button from 'react-bootstrap-button-loader';*/
+class Landing extends React.Component {
+
+routeTo = (path) => this.props.history.push(path);
+
+render(){
     return (
         <div>
-            landing page
+        <header>
+           <h1> Welcome to ChannelX </h1>
+        </header>
+         <div className="Register">
+                        <button
+                            type="button"
+                            style={{ marginLeft: "auto" }}
+                            className="Register"
+                            onClick={() => this.routeTo('/signup')}
+                            
+                        >Register</button>
         </div>
+   
+        <div className="Login">
+                        <button
+                            type="button"
+                            style={{ marginLeft: "auto" }}
+                            className="Login"
+                        >Login</button>
+        </div>
+        <div>
+
+           <p> ChannelX will make it possible to create, share, and destroy transient communication channels. These channels should be tied to users existing communication streams (e.g., SMS, e-mail, etc.) without exposing the users actual phone number or e-mail address.</p>        </div>
+
+</div>
     );
 };
-
-export default landing;
+}
+export default Landing;
