870a8d55e11ae432da54ebdb0eb4b1761a309f1d
HongfeiJu <hongfeiju@gmail.com>
2019-09-28 10:07:54 -0700

    Revert "Merge branch 'master' of https://github.com/HongfeiJu/ChannelX"
    This reverts commit 85c6d94927a6488a78208a621ff526a926a2afa8, reversing
    changes made to b2227db0926064eed3d9ee92ea1c5623b6576bee.

--- Frontend/channelx/src/Landing/Landing.js
+++ Frontend/channelx/src/Landing/Landing.js
@@ -1,49 +1,17 @@
 /*
 Description: Landing page
-Authors: Manisha Miriyala
+Authors: Hongfei Ju
 Date: 9/20/2019
 */
 
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import './Landing.css'
-import { Button} from 'reactstrap';
 
-/*import Button from 'react-bootstrap-button-loader';*/
-class Landing extends React.Component {
-
-routeTo = (path) => this.props.history.push(path);
-
-render(){
+const landing = () => {
     return (
-        <div className="wrapper">
-        <header>
-           <h1> Welcome to ChannelX </h1>
-        </header>
-         <div className="Register">
-                        <button
-                            type="button"
-                            style={{ marginLeft: "auto" }}
-                            className="Register"
-                            onClick={() => this.routeTo('/signup')}    
-                        >Register</button>
-        </div>
-   
-        <div className="Login">
-                        <button
-                            type="button"
-                            style={{ marginLeft: "auto" }}
-                            className="Login"
-                        >Login</button>
-        </div>
         <div>
-
-           <p> ChannelX will make it possible to create, share, and destroy transient communication channels. </p>
-          <p> These channels should be tied to users existing communication streams (e.g., SMS, e-mail, etc.) without exposing the users actual phone number or e-mail address.</p>     
-              </div>
-
-</div>
+            landing page
+        </div>
     );
 };
-}
-export default Landing;
+
+export default landing;
