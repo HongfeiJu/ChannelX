ed672a792621acb9ffb6a8f623a604fe3613c515
Muhammad Sami <msami2@asu.edu>
2019-10-02 15:59:45 -0700

    Improving overall structure of the project

---
+++ Frontend/channelx/src/components/Home/Home.js
@@ -0,0 +1,39 @@
+/*
+Description: Home page
+Authors: Darshan Prakash, Sami
+Date: 9/24/2019
+*/
+
+import React, {Component} from 'react';
+import fire from "../../config/Fire";
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.logout = this.logout.bind(this);
+    }
+    routeTo = (path) => this.props.history.push(path);
+
+    logout(){
+        fire.auth().signOut();
+    }
+    
+    render() {
+        return (
+            <div className="Register">
+                <button
+                    type="button"
+                    style={{ marginLeft: "auto" }}
+                    className="Logout"
+                    onClick={() => this.routeTo('/')}
+
+                >Logout</button>
+            </div>
+        );
+    }
+
+}
+
+
+
+export default Home;
