2e7217c0e4b64a35b4b8dede07e9b352e9ee0bd7
subhradeepbiswas <subhradeep.biswas@gmail.com>
2019-10-03 11:45:33 -0700

    Signup functionality removed

--- Frontend/channelx/src/Login/Login.js
+++ Frontend/channelx/src/Login/Login.js
@@ -61,18 +61,6 @@ class Login extends Component{
         })
     }
 
-    signup(e){
-        e.preventDefault();
-        fire.auth().createUserWithEmailAndPassword(this.state.email, this.state.password).then((u)=>{
-        }).then((u)=>{
-            console.log(u);
-            this.routeTo('/home');
-        }).catch((error) => {
-            console.log(error);
-        })
-    }
-
-
     render(){
         return <div className="wrapper">
             <div className="form-wrapper">
