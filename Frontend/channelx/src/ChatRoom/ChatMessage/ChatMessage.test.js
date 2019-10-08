dd4145fbf26a83f7a048e56e910a0902990a3de6
HongfeiJu <hongfeiju@gmail.com>
2019-10-08 14:47:39 -0700

    testing for chat message component

---
+++ Frontend/channelx/src/ChatRoom/ChatMessage/ChatMessage.test.js
@@ -0,0 +1,37 @@
+/*
+Description: chat message component testing
+Authors: Hongfei Ju
+Date: 10/8/2019
+*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {configure, shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16'
+import ChatMessage from "./ChatMessage";
+
+configure({adapter: new Adapter()});
+
+describe('<ChatMessage/>', ()=>{
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<ChatMessage />, div);
+        ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('should have one form element', ()=>{
+        const wrapper = shallow(<ChatMessage/>);
+        expect(wrapper.find('.message')).toHaveLength(1);
+    });
+
+    it('should have one first name input element', ()=>{
+        const wrapper = shallow(<ChatMessage/>);
+        expect(wrapper.find('.messageUser')).toHaveLength(1);
+    });
+
+    it('should have one last name input element', ()=>{
+        const wrapper = shallow(<ChatMessage/>);
+        expect(wrapper.find('.messageTimestamp')).toHaveLength(1);
+    });
+
+});
