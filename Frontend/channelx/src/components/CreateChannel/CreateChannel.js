2cf4ff0cb92cf79622118b42372fbac728e765a4
Darshan Prakash <darshan@darshans-mbp-3.3521.dhcp.asu.edu>
2019-10-18 20:35:08 -0700

    updating Create channel to handle the fields and aligning social login buttons

--- Frontend/channelx/src/components/CreateChannel/CreateChannel.js
+++ Frontend/channelx/src/components/CreateChannel/CreateChannel.js
@@ -12,22 +12,114 @@ class CreateChannel extends Component {
     // eslint-disable-next-line no-useless-constructor
     constructor(props) {
         super(props);
+        this.state = {
+            channelTitle: null,
+            channelPassword: null,
+            channelOnePassword: null,
+            channelStartDate: null,
+            channelEndDate: null,
+            channelStartTime: null,
+            channelEndTime: null,
+            errors:{
+                channelTitle: "",
+                channelPassword: "",
+                channelOnePassword: "",
+                channelStartDate: "",
+                channelEndDate: "",
+                channelStartTime: "",
+                channelEndTime: ""
+            }
+        }
     }
 
     routeTo = (path) => this.props.history.push(path);
 
     render() {
         return (
-            <div>
-                <h1>
-                    Create Your Channel
-                </h1>
-                <button id="back"
-                        type="button"
-                        className="Leave Channel"
-                        onClick={() => this.routeTo(ROUTES.HOME)}>
-                    Home
-                </button>
+            <div className="wrapper">
+                <div className="form-wrapper">
+                    <div className="FormTitle">
+                        <h1>Create your Channel</h1>
+                    </div>
+                    <form>
+                        <div className="channelTitle">
+                            <input
+                                type="text"
+                                placeholder="Title"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelPassword">
+                            <input
+                                type="text"
+                                placeholder="Password"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelOnePassword">
+                            <input
+                                type="text"
+                                placeholder="One time Password"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelStartDate">
+                            <text>
+                                Start Date
+                            </text>
+                            <input
+                                type="date"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelEndDate">
+                            <text>
+                                End Date
+                            </text>
+                            <input
+                                type="date"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelStartTime">
+                            <text>
+                                Start Time
+                            </text>
+                            <input
+                                type="time"
+                                min="00:00"
+                                max="23:59"
+                                required
+                            ></input>
+                        </div>
+                        <div className="channelEndTime">
+                            <text>
+                                End Time
+                            </text>
+                            <input
+                                type="time"
+                                min="00:00"
+                                max="23:59"
+                                required
+                            ></input>
+                        </div>
+                        <div className="createChannel">
+                            <button
+                                type="button"
+                                id="cancelButton"
+                                className="leaveButton"
+                                onClick={() => this.routeTo(ROUTES.HOME)}
+                            >Leave
+                            </button>
+                            <button
+                                type="submit"
+                                id="submitButton"
+                                className="createButton"
+                            >Create
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
