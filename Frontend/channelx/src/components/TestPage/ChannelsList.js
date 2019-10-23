d731186f0292e4ca67accb9e61e3f712ad7ed0a1
Muhammad Sami <msami2@asu.edu>
2019-10-23 12:06:02 -0700

    test log all channels created by logged in user

--- Frontend/channelx/src/components/TestPage/ChannelsList.js
+++ Frontend/channelx/src/components/TestPage/ChannelsList.js
@@ -1,7 +1,69 @@
-import React, {useState, useEffect} from 'react';
+/*
+Description: Display all channels in DB 
+Authors: Sami
+Date: 10/18/2019
+*/
+
+import React, {useState, useEffect, createElement} from 'react';
 import fire from "../../config/Fire";
 
+
+// var user = fire.auth().currentUser;
+// var ch = fire.collection('users').doc(user.uid).get().h2
+
+
+var db = fire.database();
+var ref = db.ref('channels');
+ref.on('value', gotData , errData);
+var li
+var titles = [0,1,2,3,4];
+
+const numbers = [1, 2, 3, 4, 5];
+const listItems = numbers.map((number) =>
+  <li>{number}</li>
+);
+
+function gotData(data) {
+
+    var userId = fire.auth().currentUser.uid;
+    console.log(userId);
+
+   var chan = data.val();
+//    console.log(chan);
+   var keys = Object.keys(chan);
+   console.log(keys);
+for(var i = 0; i<keys.length; i++)  {
+    // console.log(keys[i]);
+    var k = keys[i];
+    var title = chan[k].title;
+    var creator = chan[k].creator
+
+    if(creator !== userId) {
+
+        console.log(title, creator);
+        createElement('li', title );
+        console.log(li);
+        // li.class("channellisting");
+        // li.parent("channellist");
+
+        // titles = title;
+
+    } 
+
+    
+
+    
+}
+}
+
+function errData(err) {
+    console.log('error');
+    console.log(err);
+}
+
+
 function useChannels() {
+
 
     const[channels,setchannels] = useState([])
 
@@ -16,7 +78,6 @@ function useChannels() {
               }))
 
               setchannels(newChannels)
-
          })
 
 
@@ -25,26 +86,30 @@ function useChannels() {
     return channels
 }
 
+
+
+
 const ChannelsList = () => {
+
+
+  
     const channels = useChannels()
     return (
-     <div>
+     <body>
             <h2>Channel List</h2>
         
-        <ol> 
-            {channels.map((ch) => 
-            <li key = {ch.id}>
-             <div className = "Channels" >
-             <h2>{ch.Title}</h2>
-               <code className="channel">{ch.ValidDate}</code>
-               <code className="channel">{ch.ValidTime}</code>
-               <code className="channel">{ch.CreatorID}</code>
-             </div>
-            </li>
-            )}
+        <ol id = "channellist"> 
+            
+            
         </ol>
-     </div>
+     </body>
     )
 }
+
+
+  
+
+
+
 
 export default ChannelsList
