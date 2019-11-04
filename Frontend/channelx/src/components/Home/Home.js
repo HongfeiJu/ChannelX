5aa85e649f056651be4a6ff13000e44b00fb710b
subhradeepbiswas <subhradeep.biswas@gmail.com>
2019-11-03 21:31:21 -0700

    Changes in public channel dropdown

--- Frontend/channelx/src/components/Home/Home.js
+++ Frontend/channelx/src/components/Home/Home.js
@@ -146,7 +146,7 @@ class Home extends Component {
             })
             .then(data => {
                 const {query} = this.state;
-                const filteredData = data;
+                const filteredData = data.slice(0, 1);
                 this.setState({
                     data,
                     filteredData,
@@ -218,7 +218,7 @@ class Home extends Component {
                             onChange={this.handleInputChange}
                         />
                         <select id="channelDrop"
-                                size={this.state.size} onFocus={()=>{this.setState({size: 5})}} 
+                                size={this.state.size} onFocus={()=>{this.setState({size: 3})}} 
                                 onBlur={()=>{this.setState({size: 1})}} //onChange={(e)=>{e.target.blur()}}
                                 onChange={this.handleSelectChange}
                                 >
