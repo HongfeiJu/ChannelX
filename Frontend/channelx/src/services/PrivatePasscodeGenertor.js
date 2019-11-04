a27c7dc516df3b209ea5d478f36e43ab748a4a54
HongfeiJu <hongfeiju@gmail.com>
2019-11-04 14:01:02 -0700

    create a new complex unique privatePasscode generator

---
+++ Frontend/channelx/src/services/PrivatePasscodeGenertor.js
@@ -0,0 +1,36 @@
+/*
+description: generate private passcode
+author: Hongfei Ju
+date: 11/04/2019
+ */
+
+import firebase from "firebase";
+
+class PrivatePasscodeGenerator{
+
+    generatePrivatePasscode(channelID){
+        return firebase.database().ref('privatePasscodes/').once('value', (r)=>{
+            console.log(r.val());
+            const passcodes = Object.keys(r.val());
+            const years = ['1995', '1996', '1997', '1998', '1999', '2000', '2001', '2002', '2003', '2004', ],
+                colors = ['Red', 'Blue', 'Yellow', 'Orange', 'Green', 'Purple', 'Black', 'Gold', 'white', 'Silver'],
+                brands = ['Honda', 'Toyota', 'Nissan', 'Chevrolet', 'Ford', 'Jeep', 'Audi', 'Benz', 'BMW', 'Fiat'];
+            let nextPasscode = years[Math.floor(Math.random()*colors.length)]
+                +colors[Math.floor(Math.random()*colors.length)]
+                +brands[Math.floor(Math.random()*brands.length)];
+            while(passcodes.includes(nextPasscode)){
+                nextPasscode = years[Math.floor(Math.random()*colors.length)]
+                    +colors[Math.floor(Math.random()*colors.length)]
+                    +brands[Math.floor(Math.random()*brands.length)];
+            }
+            alert('generate '+nextPasscode);
+            firebase.database().ref('privatePasscodes/'+nextPasscode).set(channelID)
+                .then(r=>{
+                    alert(nextPasscode+" is added to "+channelID)
+                });
+            return nextPasscode;
+        })
+    }
+}
+
+export default PrivatePasscodeGenerator;
