b1b2caa80209aca39cdea13b82bb29fdf94ca204
Darshan Prakash <darshan@darshans-mbp-3.3521.dhcp.asu.edu>
2019-12-03 14:32:13 -0700

    Project structure clean up

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -4,6 +4,3 @@ export const ABOUTUS = '/aboutus';
 export const EMAIL_SENT = '/emailsent';
 export const CHAT_PAGE = '/channel/:id';
 export const TEST_PAGE = '/test';
-export const CREATE_CHANNEL = '/createChannel';
-export const EDIT_CHANNEL = '/editChannel';
-export const CREATE_PRIVATE_CHANNEL = '/createPrivateChannel';
